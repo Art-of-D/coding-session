8135b3d044ba59c242a3c51427e08599a65bceaf
Artur Didur <114071335+Art-of-D@users.noreply.github.com>
2024-05-01 19:37:29 +0000

    change bot

--- cmd/homeUnrealBot.go
+++ cmd/homeUnrealBot.go
@@ -13,7 +13,7 @@ import (
 var (
 	// TeleToken bot
 	TeleToken  = os.Getenv("TELE_TOKEN")
-	appVersion = "1.0.0"
+	appVersion = "1.1.0"
 )
 
 // homeUnrealBotCmd represents the homeUnrealBot command
@@ -28,25 +28,73 @@ var homeUnrealBotCmd = &cobra.Command{
 			Token:  TeleToken,
 			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 		})
+
 		if err != nil {
 			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
 		}
 
-		unrealBot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
-			payload := m.Text()
-
-			switch payload {
-			case "hi":
-				err = m.Send(fmt.Sprintf("Hello I'm UnrealBot %s!", appVersion))
+		unrealBot.Handle("/start", func(m telebot.Context) error {
+			menu := &telebot.ReplyMarkup{
+				ReplyKeyboard: [][]telebot.ReplyButton{
+					{{Text: "Hello"}, {Text: "Help"}},
+					{{Text: "Kyiv"}, {Text: "Boston"}, {Text: "London"}},
+					{{Text: "Vienna"}, {Text: "Tbilisi"}, {Text: "Vancouver"}},
+				},
 			}
-
-			return err
+			return m.Send("Welcome to Kbot!", menu)
 		})
 
+		unrealBot.Handle(telebot.OnText, func(m telebot.Context) error {
+			switch m.Text() {
+			case "Hello":
+				return m.Send(fmt.Sprintf("Hi! I'm Kbot %s! And I know what time it is!", appVersion))
+			case "Help":
+				return m.Send("This is the help message. Here you can find out the current time in the locations of your partners and team members: Kyiv, Boston, London, Vienna, Tbilisi or Vancouver")
+			case "Kyiv":
+				return m.Send("Current time in Kyiv: " + getTime("Kyiv"))
+			case "Boston":
+				return m.Send("Current time in Boston: " + getTime("Boston"))
+			case "London":
+				return m.Send("Current time in London: " + getTime("London"))
+			case "Vienna":
+				return m.Send("Current time in Vienna: " + getTime("Vienna"))
+			case "Tbilisi":
+				return m.Send("Current time in Tbilisi: " + getTime("Tbilisi"))
+			case "Vancouver":
+				return m.Send("Current time in Vancouver: " + getTime("Vancouver"))
+			default:
+				return m.Send("Unknown command. Please try again.")
+			}
+		})
 		unrealBot.Start()
 	},
+}
+
+func getTime(location string) string {
+	var locName string
+	switch location {
+	case "Kyiv":
+		locName = "Europe/Kiev"
+	case "Boston":
+		locName = "America/New_York"
+	case "London":
+		locName = "Europe/London"
+	case "Vienna":
+		locName = "Europe/Vienna"
+	case "Tbilisi":
+		locName = "Asia/Tbilisi"
+	case "Vancouver":
+		locName = "America/Vancouver"
+	default:
+		return "Invalid location"
+	}
+
+	loc, err := time.LoadLocation(locName)
+	if err != nil {
+		return "Invalid location"
+	}
+	return time.Now().In(loc).Format("15:04:05")
 }
 
 func init() {
